0aca0ca4c4369053734f5ab3eeefeed74d1390d1
Denys <ddeenis46@gmail.com>
2025-11-22 12:42:04 +0000

    tasks 1-4

--- Tasks/Day1/1-container.ts
+++ Tasks/Day1/1-container.ts
@@ -1,5 +1,33 @@
 class Integer {
   // Put implementation here
+  #value: number;
+
+  constructor(value: number) {
+    this.throwOnFloat(value);
+    this.#value = value;
+  }
+
+  public get() {
+    return this.#value;
+  }
+
+  public add(other: Integer) {
+    return new Integer(this.get() + other.get());
+  }
+
+  public div(other: Integer) {
+    return new Integer(this.get() / other.get());
+  }
+
+  public gt(other: Integer) {
+    return this.get() > other.get();
+  }
+
+  private throwOnFloat(value: number) {
+    if (value !== Math.floor(value)) {
+      throw new Error("Integer does not allow floats");
+    }
+  }
 }
 
 // Usage
@@ -10,7 +38,7 @@ const b = new Integer(3);
 const c = a.add(b);
 const d = a.div(b);
 if (a.gt(b)) {
-  console.log('a > b');
+  console.log("a > b");
 }
 
 console.log(`c = ${c.get()}`);
---
+++ Tasks/Day1/4-json.ts
@@ -0,0 +1,25 @@
+"use strict";
+
+// Rewrite to TypeScript with interface
+
+interface Serializable {
+  toJson(): string;
+}
+
+class User implements Serializable {
+  #id;
+  #name;
+
+  constructor(id: number, name: string) {
+    this.#id = id;
+    this.#name = name;
+  }
+
+  public toJson(): string {
+    return JSON.stringify({ id: this.#id, name: this.#name });
+  }
+}
+
+const user = new User(15, "Marcus");
+console.log(user.toString());
+console.log(user.toJson());
