6f485ed43f367cc260296a8d0a62a2052f4ca1c5
Sasha Mizov <oleksandr.mizov@grammarly.com>
2025-11-22 14:02:52 +0200

    Add implementation for future

--- Tasks/Day1/5-future.js
+++ Tasks/Day1/5-future.js
@@ -3,7 +3,30 @@
 const fs = require('node:fs');
 
 class Future {
-  // Put implementation here
+  // fn: (reject, resolve) => void
+  #fn
+  constructor(fn) {
+    this.#fn = fn;
+  }
+
+  map(mapFn) {
+    return new Future((reject, resolve) => {
+      this.#fn(reject, (...args) => resolve(mapFn(...args)))
+    })
+  }
+
+  chain(chainFn){
+    return new Future((reject, resolve) => {
+      this.#fn(reject, (...args) => {
+        chainFn(...args).fork(reject,resolve)
+      })
+    })
+  }
+
+  fork(reject, resolve) {
+    this.#fn(reject, resolve)
+    return this;
+  }
 }
 
 const futurify = (fn) =>
@@ -17,10 +40,36 @@ const futurify = (fn) =>
 const readFuture = futurify(fs.readFile);
 const writeFuture = futurify(fs.writeFile);
 
-readFuture('future.js', 'utf8')
+readFuture('5-future.js', 'utf8')
   .map((text) => text.toUpperCase())
   .chain((text) => writeFuture('future.md', text))
   .fork(
     (error) => console.error('FS error:', error),
-    () => console.log('Done'),
+    () => {
+      console.log('Done')
+    },
   );
+
+// Laziness test
+
+// console.log('before creation')
+// const lazy = readFuture('5-future.js', 'utf8')
+//   .map((text) => {
+//     console.log('map')
+//     return text.toUpperCase()
+//   })
+//   .chain((text) => {  
+//     console.log('cahin')
+//     return writeFuture('future.md', text)
+//   }
+// )
+// console.log('post creation')
+
+// lazy.fork(
+//     (error) => console.error('FS error:', error),
+//     () => {
+//       console.log('Done')
+//     },
+//   );
+
+// console.log('post fork')
