58de753f78b301cbb6771e04d64aa378c408bd89
Sasha Mizov <oleksandr.mizov@grammarly.com>
2025-11-22 14:24:20 +0200

    Add do notation solution

--- Tasks/Day1/2-do.js
+++ Tasks/Day1/2-do.js
@@ -1,8 +1,34 @@
 'use strict';
 
 // Put implementation here
+class Do {
+  #value
 
-Do({ id: 15 })
+  constructor(value) {
+    this.#value = value;
+  }
+
+  // static from(value){
+  //   return new Do(value)
+  // }
+
+  bind(fn) {
+    this.#value = fn(this.#value)
+    return this
+  }
+
+  run() {
+    return fn => {
+      if (typeof this.#value !== 'function') {
+        return
+      }
+      this.#value(fn)
+    }
+  }
+
+}
+
+new Do({ id: 15 })
   .bind(({ id }) => ({ id, name: 'marcus', age: 42 }))
-  .bind(({ name, age }) => (name === 'marcus' ? (log) => log(age) : () => {}))
+  .bind(({ name, age }) => (name === 'marcus' ? (log) => log(age) : () => { }))
   .run()(console.log);
