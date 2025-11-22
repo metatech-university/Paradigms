7c2f80152fe7c2fe90bd63365fad691d713b654b
Sasha Mizov <oleksandr.mizov@grammarly.com>
2025-11-22 15:11:57 +0200

    New execute

--- Tasks/Day1/3-execute.js
+++ Tasks/Day1/3-execute.js
@@ -2,32 +2,93 @@
 
 const reader = ({ id }) => ({ id, name: 'marcus', age: 42 });
 
-const execute = (plan) => (reader, log, env = {}) => {
-  if (plan.read) {
-    const user = reader(plan.read);
-    return execute(plan.then)(reader, log, { user });
-  }
-  if (plan.match) {
-    const ok = env.user.name === plan.match.name;
-    return execute(ok ? plan.success : plan.fail)(reader, log, env);
-  }
-  if (plan.effect) {
-    if (plan.effect.log) return () => log(env.user[plan.effect.log]);
-    if (plan.effect === 'noop') return () => {};
-  }
-};
+// const execute = (plan) => (reader, log, env = {}) => {
+//   if (plan.read) {
+//     const user = reader(plan.read);
+//     return execute(plan.then)(reader, log, { user });
+//   }
+//   if (plan.match) {
+//     const ok = env.user.name === plan.match.name;
+//     return execute(ok ? plan.success : plan.fail)(reader, log, env);
+//   }
+//   if (plan.effect) {
+//     if (plan.effect.log) return () => log(env.user[plan.effect.log]);
+//     if (plan.effect === 'noop') return () => { };
+//   }
+// };
 
-execute({
-  read: { id: 15 },
-  then: {
-    match: { name: 'marcus' },
-    success: { effect: { log: 'age' } },
-    fail: { effect: 'noop' },
+class Executor {
+
+  #reader
+  #logger
+  constructor(reader, logger) {
+    this.#logger = logger
+    this.#reader = reader
+  }
+
+  execute(instructions) {
+    instructions.reduce((env, instruction) => this.executeIntruction(env, instruction), {})
+  }
+
+  executeIntruction(env, instruction) {
+    const { type, params, effect } = instruction
+
+    const opEcecutor = this[type].bind(this)
+    return opEcecutor === undefined ? env : opEcecutor(env, effect, ...params)
+  }
+
+  read(ctx, { fail, success }, param) {
+    try {
+      const value = this.#reader(param)
+      if (success) this.executeIntruction(value, success)
+
+      return value
+    } catch (e) {
+      if (fail) this.executeIntruction(e, fail)
+      return ctx
+    }
+  }
+
+  match(ctx, { fail, success }, matchParam) {
+    const key = Object.keys(matchParam)[0]
+    if (!key) return fail()
+
+    if (ctx[key] === matchParam[key]) {
+      this.executeIntruction(ctx, success)
+      return ctx
+    }
+    this.executeIntruction(ctx, fail)
+  }
+
+
+  noop() {
+  }
+
+  log(ctx, _effects, param) {
+    const key = Object.keys(param)[0]
+    const val = ctx[key]
+    this.#logger(val)
+  }
+
+}
+
+const Operation = Object.freeze({
+  noop: () => ({ type: 'noop', params: [], effect: {} }),
+  log: key => ({ type: 'log', params: [{ [key]: key }], effect: {} }),
+  read: ({ params, effect }) => ({ type: 'read', params, effect }),
+  match: ({ params, effect }) => ({ type: 'match', params, effect })
+})
+
+new Executor(reader, console.log).execute([
+  {
+    type: 'read',
+    params: [{ id: 15 }],
+    effect: { fail: Operation.noop(), success: Operation.log('name') }
   },
-})(reader, console.log)();
+  {
+    type: 'match',
+    params: [{ name: 'marcus' }],
+    effect: { fail: Operation.noop(), success: Operation.log('age') }
+  }
+]);
 
-// 1. Rewrite in OOP style
-// 2. Improve data structure inconsistence
-
-// const main = new Exec(options);
-// main.run(steps);
