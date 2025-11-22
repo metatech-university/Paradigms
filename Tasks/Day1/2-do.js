9efe7840cfe118542b9b2ec9c675b7e46bb9344a
Rys Vladislav <onessse@gmail.com>
2025-11-22 15:36:55 +0100

    day 1: tasks(1-4)

--- Tasks/Day1/2-do.js
+++ Tasks/Day1/2-do.js
@@ -1,6 +1,16 @@
 'use strict';
 
 // Put implementation here in class syntax
+const Do = (value) => ({
+  value,
+  bind(fn) {
+    const evaluate = typeof value === 'function' ? value : () => value;
+    return Do(() => fn(evaluate()));
+  },
+  run() {
+    return typeof value === 'function' ? value() : value;
+  }
+});
 
 Do({ id: 15 })
   .bind(({ id }) => ({ id, name: 'marcus', age: 42 }))
