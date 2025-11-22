f190a5284ba9d1c14404a528eab1609bf5196057
D N <4661784+retyui@users.noreply.github.com>
2025-11-22 12:50:26 +0100

    Add "do" implementation

--- Tasks/Day1/2-do.js
+++ Tasks/Day1/2-do.js
@@ -1,6 +1,14 @@
 'use strict';
 
-// Put implementation here
+const Do = (value) => {
+    const binds = [];
+    const run = () => binds.reduce((acc, fn) => fn(acc), value);
+    const bind = fn => {
+        binds.push(fn);
+        return {bind,run};
+    }
+    return { bind };
+}
 
 Do({ id: 15 })
   .bind(({ id }) => ({ id, name: 'marcus', age: 42 }))
