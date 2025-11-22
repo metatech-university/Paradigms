351f1ef00e1f7a2efa9f3f0df50a10039dab5df5
rhordiichuk <rhordiichuk@gmail.com>
2025-11-22 16:12:17 +0100

    Hordiichuk Roman: completed 4 tasks from day 1

--- Tasks/Day1/1-container.ts
+++ Tasks/Day1/1-container.ts
@@ -1,5 +1,27 @@
 class Integer {
-  // Put implementation here
+   #value: number;
+
+  constructor(value: number) {
+    this.#value = value;
+  }
+
+  public add(newValue: Integer) {
+    this.#value += newValue.get();
+    return new Integer(this.#value);
+  }
+
+  public div(newValue: Integer) {
+    this.#value /= newValue.get();
+    return new Integer(this.#value);
+  }
+
+  public gt(newValue: Integer) {
+    return this.#value > newValue.get();
+  }
+
+  public get() {
+    return this.#value;
+  }
 }
 
 // Usage
