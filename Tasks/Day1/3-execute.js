6c9ac4d427e0a80979f569f94fbf34c20108d0d1
D N <4661784+retyui@users.noreply.github.com>
2025-11-22 13:25:27 +0100

    Add "execute" implementation

--- Tasks/Day1/3-execute.js
+++ Tasks/Day1/3-execute.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const reader = ({ id }) => ({ id, name: 'marcus', age: 42 });
+const reader = ({id}) => ({id, name: 'marcus', age: 42});
 
 const execute = (plan) => (reader, log, env = {}) => {
   if (plan.read) {
@@ -29,5 +29,48 @@ execute({
 // 1. Rewrite in OOP style
 // 2. Improve data structure inconsistence
 
-// const main = new Exec(options);
-// main.run(steps);
+/**
+ * Exec orchestrates plan execution.
+ *
+ * @class
+ * @property {(input: any) => any} reader
+ * @property {(msg: any) => void} log
+ */
+class Exec {
+    constructor(options) {
+        Object.assign(this, options);
+        return this;
+    }
+
+    run(plan, data = {}) {
+        if (plan.type === 'read') {
+            return this.run(plan.next, this.reader(plan.value));
+        }
+        if (plan.type === 'match') {
+            const success = plan.value.name === data.name;
+            return this.run(success ? plan.next[0] : plan.next[1], data);
+        }
+        if (plan.type === 'log') {
+            return this.log(data[plan.value]);
+        }
+        if (plan.type === 'noop') {
+            return;
+        }
+
+        throw new Error('Unknown plan type');
+    }
+}
+
+const main = new Exec({
+    reader,
+    log: console.log,
+});
+main.run({
+    type: 'read',
+    value: {id: 15},
+    next: {
+        type: 'match',
+        value: {name: 'marcus'},
+        next: [{type: 'log', value: 'age'}, {type: 'noop'}]
+    },
+});
