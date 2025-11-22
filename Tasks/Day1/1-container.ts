57a7668b4eb0f14d86e484f83cec9945f059f0f8
Sasha Mizov <oleksandr.mizov@grammarly.com>
2025-11-22 14:24:06 +0200

    Add container solution

--- Tasks/Day1/1-container.ts
+++ Tasks/Day1/1-container.ts
@@ -1,12 +1,31 @@
 class Integer {
-  // Put implementation here
+  #value: number
+  constructor(a: number){
+    // todo we might want to check the type before assingment
+    this.#value = a
+  }
+  add(a: Integer){
+    return new Integer(this.#value + a.#value)
+  }
+
+  div(a: Integer){
+    // todo we might want to handle div by 0
+    return new Integer(this.#value/a.#value)
+  }
+
+  gt(a:Integer){
+    return this.#value > a.#value
+  }
+
+  get(): number{
+    return this.#value;
+  }
 }
 
 // Usage
 
 const a = new Integer(7);
 const b = new Integer(3);
-
 const c = a.add(b);
 const d = a.div(b);
 if (a.gt(b)) {
