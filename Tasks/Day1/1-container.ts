dac9df29492b05ab866fc3f61d961abe6f655d63
Viktor Hrimli <viktorhrimli101@gmail.com>
2025-11-22 14:55:42 +0200

    Implement Integer and Do classes with basic operations

--- Tasks/Day1/1-container.ts
+++ Tasks/Day1/1-container.ts
@@ -1,5 +1,29 @@
 class Integer {
-  // Put implementation here
+  value: number;
+
+  constructor(value: number) {
+    if (typeof value !== "number" || isNaN(value)) {
+      throw new Error("Value must be a number");
+    }
+    this.value = value;
+  }
+
+  add(other: Integer): Integer {
+    return new Integer(this.value + other.get());
+  }
+
+  div(other: Integer): Integer {
+    return new Integer(Math.floor(this.value / other.get()));
+  }
+
+  get(): number {
+    return this.value;
+  }
+
+  gt(other: Integer): boolean {
+    const valueOther = other.get();
+    return this.value > valueOther;
+  }
 }
 
 // Usage
@@ -10,7 +34,7 @@ const b = new Integer(3);
 const c = a.add(b);
 const d = a.div(b);
 if (a.gt(b)) {
-  console.log('a > b');
+  console.log("a > b");
 }
 
 console.log(`c = ${c.get()}`);
