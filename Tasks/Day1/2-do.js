32623e182e1c0e4e06a4347b5fe5cf4cbe70aeeb
Peter Shumskiy <fwshumskiy@gmail.com>
2025-11-22 23:13:33 +0100

    task 1-4

--- Tasks/Day1/2-do.js
+++ Tasks/Day1/2-do.js
@@ -1,8 +1,41 @@
-'use strict';
+"use strict";
+
+// must evaluate lazy
+class DoImplementation {
+  #params = [];
+  #tasks = [];
+
+  constructor(initialParams = {}) {
+    this.#params.push(initialParams);
+  }
+
+  bind(taskExecutor) {
+    this.#tasks.push((cb) => {
+      const result = taskExecutor(this.#params.pop());
+      if (typeof result === "function") {
+        result(cb);
+      } else {
+        this.#params.push(result);
+      }
+    });
+
+    return this;
+  }
+
+  run() {
+    return (cb) => {
+      this.#tasks.forEach((task) => task(cb));
+    };
+  }
+}
+
+function Do(initialParams = {}) {
+  return new DoImplementation(initialParams);
+}
 
 // Put implementation here
 
 Do({ id: 15 })
-  .bind(({ id }) => ({ id, name: 'marcus', age: 42 }))
-  .bind(({ name, age }) => (name === 'marcus' ? (log) => log(age) : () => {}))
+  .bind(({ id }) => ({ id, name: "marcus", age: 42 }))
+  .bind(({ name, age }) => (name === "marcus" ? (log) => log(age) : () => {}))
   .run()(console.log);
