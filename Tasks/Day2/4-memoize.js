f055ca1e7fabf4e7c0a45f9455cfa7a9d7bcac81
Sasha Mizov <oleksandr.mizov@grammarly.com>
2025-11-23 14:31:20 +0200

    Add memoization with type key builder

--- Tasks/Day2/4-memoize.js
+++ Tasks/Day2/4-memoize.js
@@ -2,22 +2,20 @@
 
 // implement memoize
 const defaultKeyBuilder = (...args) => args.join(',')
+const keyBuilderWithTypes = (...args) => args.map(a => `${a.toString()}-${typeof a}`).join(',')
 
 const memoize = (f, keyBuilder = defaultKeyBuilder) => {
     const cache = new Map();
     return (...args) => {
         const key = keyBuilder(...args)
         if (cache.get(key) == null) {
-            // console.log(`calculate: ${key}`);
             const res = f(...args)
             cache.set(key, res)
-        } // else {
-        //     console.log(`cache hit: ${key}`);
-        // }
+        }
         return cache.get(key);
     }
 };
 
-const fib = memoize((n) => n <= 1 ? n : fib(n - 1) + fib(n - 2));
+const fib = memoize((n) => n <= 1 ? n : fib(n - 1) + fib(n - 2), keyBuilderWithTypes);
 
 console.log(fib(10));
