2b4277378d553e0834292baace507855e3a44757
almaceleste <pcaptanovska@gmail.com>
2025-11-23 14:40:13 +0100

    refactor: simplify

--- Tasks/Day2/4-memoize.js
+++ Tasks/Day2/4-memoize.js
@@ -4,15 +4,12 @@
 
 // const memoize = (f) => f;
 const cache = new Map();
-const memoize = (f) => {
-
-  return (...args) => {
-    const key = args.map((arg) => `${arg}:${typeof arg}`).join(';');
-    if (cache.has(key)) return cache.get(key);
-    const res = f(...args);
-    cache.set(key, res);
-    return res;
-  };
+const memoize = (f) => (...args) => {
+  const key = args.map((arg) => `${arg}:${typeof arg}`).join(';');
+  if (cache.has(key)) return cache.get(key);
+  const res = f(...args);
+  cache.set(key, res);
+  return res;
 };
 
 const fib = memoize((n) => n <= 1 ? n : fib(n - 1) + fib(n - 2));
