441d23c46fc137cd2c72717fc92cc288e8daf229
holovko.r <holovko.r@novaposhta.ua>
2025-11-23 12:18:53 +0200

    Implement memoization

--- Tasks/Day2/4-memoize.js
+++ Tasks/Day2/4-memoize.js
@@ -1,8 +1,14 @@
 'use strict';
 
 // implement memoize
+const memoize = (fn) => {
+    const cache = new Map();
 
-const memoize = (f) => f;
+    return (arg) => {
+        if (!cache.has(arg)) cache.set(arg, fn(arg));
+        return cache.get(arg);
+    }
+};
 
 const fib = memoize((n) => n <= 1 ? n : fib(n - 1) + fib(n - 2));
 
