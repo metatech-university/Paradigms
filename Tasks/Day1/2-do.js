d9219d2ca21c850fe7ddb9db20d1432e757f2393
Serhii Yanhol <serhii@airwayz,co>
2025-11-23 09:20:10 +0200

    Day 1: tasks 1-4

--- Tasks/Day1/2-do.js
+++ Tasks/Day1/2-do.js
@@ -1,6 +1,23 @@
 'use strict';
 
-// Put implementation here
+class DoMonad {
+	#value;
+
+	constructor(value) {
+		this.#value = value;
+	}
+
+	bind(fn) {
+		const next = fn(this.#value);
+		return new DoMonad(next);
+	}
+
+	run() {
+		return typeof this.#value === 'function' ? this.#value : () => this.#value;
+	}
+}
+
+const Do = (value) => new DoMonad(value);
 
 Do({ id: 15 })
   .bind(({ id }) => ({ id, name: 'marcus', age: 42 }))
