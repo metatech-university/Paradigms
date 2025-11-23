8b583167ae3f28e737b99f09b0a9e37a188ad6f7
Sasha Mizov <oleksandr.mizov@grammarly.com>
2025-11-23 11:30:53 +0200

    Solve task 2-closure

--- Tasks/Day2/2-closure.js
+++ Tasks/Day2/2-closure.js
@@ -3,21 +3,14 @@
 // Rewrite previous example from OOP with mutable state
 // to FP using closure-based syntax, immutable instance, method chaining
 
-class Adder {
-  constructor(initial) {
-    this.value = initial;
-  }
-
-  add(x) {
-    this.value += x;
-    return this;
-  }
-
-  valueOf() {
-    return this.value;
+function createAdder(_value) {
+  const value = _value;
+  return {
+    add: a => createAdder(a + value),
+    valueOf: () => value
   }
 }
 
-const sum1 = new Adder(1).add(9).add(1).add(7);
-// TODO: sum1 = createAdder(1).add(9).add(1).add(7);
+// const sum1 = new Adder(1).add(9).add(1).add(7);
+const sum1 = createAdder(1).add(9).add(1).add(7);
 console.log('Sum:', +sum1);
