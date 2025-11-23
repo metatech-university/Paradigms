3a5c0823567d2be553adfd8d2d88a58774bfa2fd
Oleksandr Omelchenko <oleksandr.o@saasjet.com>
2025-11-23 12:50:51 +0200

    tasks ready

--- Tasks/Day2/2-closure.js
+++ Tasks/Day2/2-closure.js
@@ -3,21 +3,10 @@
 // Rewrite previous example from OOP with mutable state
 // to FP using closure-based syntax, immutable instance, method chaining
 
-class Adder {
-  constructor(initial) {
-    this.value = initial;
-  }
+const createAdder = (initial) => ({
+  add: (x) => createAdder(initial + x),
+  valueOf: () => initial,
+});
 
-  add(x) {
-    this.value += x;
-    return this;
-  }
-
-  valueOf() {
-    return this.value;
-  }
-}
-
-const sum1 = new Adder(1).add(9).add(1).add(7);
-// TODO: sum1 = createAdder(1).add(9).add(1).add(7);
+const sum1 = createAdder(1).add(9).add(1).add(7);
 console.log('Sum:', +sum1);
--- Tasks/Day2/4-memoize.js
+++ Tasks/Day2/4-memoize.js
@@ -2,7 +2,16 @@
 
 // implement memoize
 
-const memoize = (f) => f;
+const memoize = (f) => {
+  const cache = new Map();
+
+  return (x) => {
+    if (cache.has(x)) return cache.get(x);
+    const result = f(x);
+    cache.set(x, result);
+    return result;
+  };
+};
 
 const fib = memoize((n) => n <= 1 ? n : fib(n - 1) + fib(n - 2));
 
